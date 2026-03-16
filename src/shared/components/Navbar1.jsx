70d0c0ce9b1080a513fede61600e764ec9f6c8fc
jayjosh-glitch <jayant0913@gmail.com>
2026-03-16 21:16:15 +0530

    App V2.0

--- src/shared/components/Navbar1.jsx
+++ src/shared/components/Navbar1.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import '../components/navbar1.css';
-import logo from '../../images/logo1.png'
+import logo from '../../images/Fintracker.png'
+
 const Navbar1 = () => {
+
 	return (
 		<nav className='nav-bar'>
         <div className='title-logo'>
-            <h1>FinTracker</h1>
             <img src={logo} alt="app-logo" className='logo' />
+            <h1>FinTracker</h1>
         </div>
         <div className='nav-links'>
             <Link to='/auth/login'>Login</Link>
